62e23c487dac783b72001c1bcd95578d11172e78
kultingen <gustaf.t.bohlin@gmail.com>
2016-03-09 21:33:53 +0100

    Hello

---
+++ src/playground/seb/uml/HelloFromGurraB.java
@@ -0,0 +1,10 @@
+package playground.seb.uml;
+
+/**
+ * Created by Gustaf on 09/03/2016.
+ */
+public class HelloFromGurraB {
+    public static void main(String[] args) {
+        System.out.println("HELLO TEAM");
+    }
+}
