31d73afffd34bd2f76bc042c8e31a2b0bdd58a14
Coweete <Jonatan_fridsten@hotmail.com>
2016-03-10 15:26:43 +0100

    Lägger upp början av GUi

---
+++ src/Gui/Gui.java
@@ -0,0 +1,24 @@
+package Gui;
+
+import playground.seb.uml.impl.Controller;
+
+import javax.swing.*;
+
+/**
+ * Created by jonatan Fridsten on 2016-03-10.
+ */
+public class Gui extends JPanel {
+    private Controller ctrl;
+
+    public Gui(){
+
+    }
+
+    public void setCtrl(Controller ctrl){
+        this.ctrl = ctrl;
+    }
+
+    public static void main(String [] args){
+
+    }
+}
