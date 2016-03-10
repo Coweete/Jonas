f5be0c0098a911c39c0a5c6d36707404cb235791
Coweete <Jonatan_fridsten@hotmail.com>
2016-03-10 10:06:56 +0100

    Hello from the other side

---
+++ src/View/HelloFromTheOtherSide.java
@@ -0,0 +1,10 @@
+package View;
+
+/**
+ * Created by jonatan on 2016-03-10.
+ */
+public class HelloFromTheOtherSide {
+    public static void main(String [] args){
+        System.out.println("Hello its me");
+    }
+}
