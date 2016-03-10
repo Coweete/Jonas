1dda00da39266294ebd36bed05d4d56dc5eb3115
Shvedi <jon_sona@yahoo.com>
2016-03-10 15:49:48 +0100

    Member done(?)

--- src/playground/seb/uml/impl/Member.java
+++ src/playground/seb/uml/impl/Member.java
@@ -1,11 +1,45 @@
 package playground.seb.uml.impl;
+import collections.ArrayList;
 
 /**
- * Created by Sebastian Börebäck on 2016-03-09.
+ * Created by Johnatan Sona on 2016-03-09.
  */
 public class Member {
-	private ArrayList loan;
+	private ArrayList<Media> loan;
 	private String memberID;
 	private String name;
 	private String phoneNumber;
+
+	public Member(String memberID, String name, String phoneNumber){
+		this.memberID = memberID;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		loan = new ArrayList<>();
+
+	}
+
+	public ArrayList getLoan() {
+		return loan;
+	}
+
+	public String getMemberID() {
+		return memberID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void loanMedia(Media mediaItem){
+		loan.add(mediaItem);
+	}
+
+	public boolean returnMedia(Media mediaItem){
+		return loan.remove(loan.indexOf(mediaItem)) != null;
+	}
+
 }
