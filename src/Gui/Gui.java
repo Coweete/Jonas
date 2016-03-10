9435ec503ca23d12bfb6d02d355d29969c6a6582
kultingen <gustaf.t.bohlin@gmail.com>
2016-03-10 17:41:12 +0100

    Created MediaService

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -8,11 +8,13 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by jonatan Fridsten on 2016-03-10.
  */
-public class Gui extends JFrame {
+public class Gui extends JFrame implements ActionListener {
     private Controller ctrl;
 
     public Gui(){
@@ -21,20 +23,20 @@ public class Gui extends JFrame {
 
     private JPanel basicJPanel(String labelInfo, JTextField textField,JButton button, String borderString) {
         JPanel panel = new JPanel(new BorderLayout());
-        JPanel panel1 = new JPanel();
+        JPanel panelButton = new JPanel();
+        JPanel panelLabel = new JPanel();
         JLabel label = new JLabel(labelInfo);
-        EmptyBorder padding = new EmptyBorder(5, 5, 5, 5);//detta e cancer
-        label.setBorder(padding);
-        textField.setBorder(padding);
-        panel1.add(button);
-        button.setBorder(padding);
+        panelButton.add(button);
+        panelLabel.add(label);
+
         TitledBorder border = new TitledBorder(BorderFactory.createLineBorder(Color.BLACK), borderString);
-        panel.add(label, BorderLayout.WEST);
+        panel.add(panelLabel, BorderLayout.WEST);
         panel.add(textField, BorderLayout.CENTER);
-        panel.add(panel1, BorderLayout.EAST);
+        panel.add(panelButton, BorderLayout.EAST);
         panel.setBorder(border);
         return panel;
     }
+
     public JPanel userPanel(){
         JPanel userPanel = new JPanel(new BorderLayout());
         JPanel titlePanel = new JPanel();
@@ -64,5 +66,18 @@ public class Gui extends JFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.pack();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {    //bara byta ut e mot knappens riktiga namn
+        if(e.getSource() == e) {        //byta login
+            ctrl.login();
+        }
+        else if(e.getSource() == e) {   //låna
+            ctrl.borrow(textFieldBorrow.getText());
+        }
+        else if(e.getSource() == e) {   //lämna tillbaka
+            ctrl.returnBook(textFieldBorrow.getText();
+        }
     }
 }
