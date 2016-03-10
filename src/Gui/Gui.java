0313fb40c3e61663eabcea918f4afe60e4ecd3a4
kultingen <gustaf.t.bohlin@gmail.com>
2016-03-10 16:52:41 +0100

    Created MediaService

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -3,6 +3,10 @@ package Gui;
 import playground.seb.uml.impl.Controller;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
 
 /**
  * Created by jonatan Fridsten on 2016-03-10.
@@ -14,11 +18,33 @@ public class Gui extends JFrame {
 
     }
 
+    private JPanel basicJPanel(String labelInfo, JTextField textField,JButton button, String borderString) {
+        JPanel panel = new JPanel(new BorderLayout());
+        JPanel panel1 = new JPanel();
+        JLabel label = new JLabel(labelInfo);
+        EmptyBorder padding = new EmptyBorder(5, 5, 5, 5);
+        label.setBorder(padding);
+        textField.setBorder(padding);
+        panel1.add(button);
+        button.setBorder(padding);
+        TitledBorder border = new TitledBorder(BorderFactory.createLineBorder(Color.BLACK), borderString);
+        panel.add(label, BorderLayout.WEST);
+        panel.add(textField, BorderLayout.CENTER);
+        panel.add(panel1, BorderLayout.EAST);
+        panel.setBorder(border);
+        return panel;
+    }
+
     public void setCtrl(Controller ctrl){
         this.ctrl = ctrl;
     }
 
     public static void main(String [] args){
-
+        Gui gui = new Gui();
+        JFrame frame = new JFrame();
+        frame.add(gui.basicJPanel("cancer", new JTextField(20), new JButton("cancer"), "cancer"));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.pack();
     }
 }
